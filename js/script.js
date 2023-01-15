44d77e46114938475b298683dbcedc8f9783c29b
Tamara Ferreira <tamarablacksmith@hotmail.com>
2023-01-15 16:24:57 -0300

    incrementa logica de firstCard e secondCard

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-//renderização dos cards no jogo. Criando arrays com as imagens.
+const card = document.querySelectorAll("card");
+let firstCard, secondCard;
 
+//renderização dos cards no jogo. Criando arrays com as imagens.
 const deskCard = document.querySelector("#desk");
 const imagens = [
     'parrot1.gif',
@@ -15,9 +17,9 @@ let cardHTML = '';
 
 imagens.forEach(img => {
     cardHTML += `
-        <div class="card" onclick="flipCard(this)">
-            <img src="img/back.png" class="frente">
-            <img src="img/${img}" class="costas">
+        <div class="card" onclick="flipCard(this)" data-test="card">
+            <img src="img/back.png" class="costas" data-test="face-down-image">
+            <img src="img/${img}" class="frente" data-test="face-up-image">
         </div>
     `
 });
@@ -27,4 +29,13 @@ deskCard.innerHTML = cardHTML + cardHTML;
 //adicionda e faz com que a carta flipe ao receber a classe 'selected'
 function flipCard(card) {
     card.classList.toggle('selected');
+    card.classList.add('selected');
+
+    if(!firstCard){
+        firstCard = card;
+
+        return false;
+    }
+
+    secondCard = card;
 }
